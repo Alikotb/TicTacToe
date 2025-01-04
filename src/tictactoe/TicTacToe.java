6f09820891e4bed263ce56177d79d3520327575e
uFayad <youssefadelfayad@gmail.com>
2025-01-04 15:03:37 +0200

    create splash screen, create ui package

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tictactoe;
 
+import tictactoe.ui.Home;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tictactoe.ui.Splash;
 
 /**
  *
@@ -19,9 +17,9 @@ public class TicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = new Home(stage);
+        Parent root = new Splash(stage);
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root,800,600);
         
         stage.setScene(scene);
         stage.show();
