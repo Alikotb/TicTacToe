efbce18b9c2e36a36ef1e1dd2cec87a09c1328f3
uFayad <youssefadelfayad@gmail.com>
2025-01-14 20:43:02 +0200

    fix delay sound and freezing board ui

--- src/tictactoe/domain/usecases/PlaySoundUseCase.java
+++ src/tictactoe/domain/usecases/PlaySoundUseCase.java
@@ -2,76 +2,76 @@ package tictactoe.domain.usecases;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.*;
 
 public class PlaySoundUseCase {
 
-    private static final File O_SOUND  = new File("src/resources/sounds/oTone.wav");
-    private static final File X_SOUND  = new File("src/resources/sounds/xTone.wav");
-    private static final File TIMER_SOUND  = new File("src/resources/sounds/timer.wav");
-    private static final File WIN_SOUND  = new File("src/resources/sounds/win2.wav");
-    private static final File DRAW_SOUND  = new File("src/resources/sounds/draw.wav");
-    private static final File LOSE_SOUND  = new File("src/resources/sounds/lose.wav");
+    private static final File O_SOUND = new File("src/resources/sounds/oTone.wav");
+    private static final File X_SOUND = new File("src/resources/sounds/xTone.wav");
+    private static final File TIMER_SOUND = new File("src/resources/sounds/timer.wav");
+    private static final File WIN_SOUND = new File("src/resources/sounds/win2.wav");
+    private static final File DRAW_SOUND = new File("src/resources/sounds/draw.wav");
+    private static final File LOSE_SOUND = new File("src/resources/sounds/lose.wav");
 
-    private Clip clip;
+    private final Map<Integer, Clip> soundClips = new HashMap<>();
 
-    public void playSound(int soundNum) {
-        File filePath;
-
-        switch (soundNum) {
-            case 1:
-                filePath = O_SOUND;
-                break;
-            case 2:
-                filePath = X_SOUND;
-                break;
-            case 3:
-                filePath = TIMER_SOUND;
-                break;
-            case 4:
-                filePath = WIN_SOUND;
-                break;
-            case 5:
-                filePath = DRAW_SOUND;
-                break;
-            case 6:
-                filePath = LOSE_SOUND;
-                break;
-            default:
-                System.err.println("Invalid sound name: " + soundNum);
-                return;
-        }
-
-        playSoundFile(filePath);
+    public PlaySoundUseCase() {
+        preloads();
     }
 
-    private void playSoundFile(File filePath) {
-        try {
-            if (clip != null && clip.isOpen()) {
-                clip.stop();
-                clip.close();
-            }
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(filePath);
-            clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.setFramePosition(0);
-            clip.start();
+    private void preloads() {
+        preloadSound(1, O_SOUND);
+        preloadSound(2, X_SOUND);
+        preloadSound(3, TIMER_SOUND);
+        preloadSound(4, WIN_SOUND);
+        preloadSound(5, DRAW_SOUND);
+        preloadSound(6, LOSE_SOUND);
+    }
 
+    private void preloadSound(int soundNum, File soundFile) {
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            soundClips.put(soundNum, clip);
         } catch (LineUnavailableException | IOException | UnsupportedAudioFileException ex) {
             Logger.getLogger(PlaySoundUseCase.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Failed to preload sound: " + soundFile.getPath());
         }
     }
-    
-    public void stopSound() {
-        if (clip.isRunning()) {
+
+    public void playSound(int soundNum) {
+        Clip clip = soundClips.get(soundNum);
+        if (clip != null) {
+            new Thread(() -> {
+                if (clip.isRunning()) {
+                    clip.stop();
+                }
+                clip.setFramePosition(0); 
+                clip.start();
+            }).start();
+        } else {
+            System.err.println("Invalid sound number: " + soundNum);
+        }
+    }
+
+    public void stopSound(int soundNum) {
+        Clip clip = soundClips.get(soundNum);
+        if (clip != null && clip.isRunning()) {
             clip.stop();
         }
     }
 
     public void closeAll() {
-        clip.close();
+        soundClips.values().forEach(clip -> {
+            if (clip.isOpen()) {
+                clip.close();
+            }
+        });
+        soundClips.clear();
     }
-
 }
