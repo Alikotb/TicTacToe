8e781bd27ec1d667808de1d5e79bbe102146c1eb
uFayad <youssefadelfayad@gmail.com>
2025-01-15 21:32:29 +0200

    create replay-game functionality in offline mode

--- src/tictactoe/ui/screens/NewGame1Base.java
+++ src/tictactoe/ui/screens/NewGame1Base.java
@@ -157,7 +157,7 @@ public class NewGame1Base extends BorderPane {
         History.setId("HISTORY");
         History.setOnAction(e -> {
             Stage historyStage = new Stage();
-            historyStage.setScene(new Scene(new History()));
+            historyStage.setScene(new Scene(new History(historyStage,mystage)));
             historyStage.show();
         });
 
